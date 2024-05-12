4734c2bc4401c9271087225a75701eea60618562
Rubick1337 <poss1337@gmail.com.>
2024-05-12 19:16:35 +0300

    feat: add comm  dart/light theme in localstorage

--- js_files/dark-light-theme.js
+++ js_files/dark-light-theme.js
@@ -1,10 +1,23 @@
 const switchInput = document.querySelector('.switch-input');
 const body = document.body;
 
+// Проверяем, есть ли сохраненное значение в локальном хранилище
+const isDarkThemeEnabled = localStorage.getItem('darkThemeEnabled') === 'true';
+
+// Устанавливаем начальное значение в соответствии с сохраненным значением
+if (isDarkThemeEnabled) {
+  body.classList.add('dark-theme');
+  switchInput.checked = true;
+}
+
 switchInput.addEventListener('change', function() {
   if (this.checked) {
     body.classList.add('dark-theme');
+    // Сохраняем значение в локальном хранилище при изменении
+    localStorage.setItem('darkThemeEnabled', 'true');
   } else {
     body.classList.remove('dark-theme');
+    // Сохраняем значение в локальном хранилище при изменении
+    localStorage.setItem('darkThemeEnabled', 'false');
   }
 });
