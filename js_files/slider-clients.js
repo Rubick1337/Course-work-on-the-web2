697e5abe34268d418d0d12adc830e8e6ad2633c4
Rubick1337 <poss1337@gmail.com.>
2024-05-13 01:20:11 +0300

    feat: ru and eng slide clients

---
+++ js_files/slider-clients.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var clients;
+  
+    fetch('/json/slider-clients.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Ой, ошибка в fetch: ' + response.statusText);
+        }
+        return response.json();
+      })
+      .then(jsonData => {
+        var language;
+        if (window.location.hash === '#ru') {
+          language = 'ru';
+        } else {
+          language = 'en';
+        }
+        clients = jsonData[language];
+        updateContent(currentClientIndex);
+      })
+      .catch(error => {
+        console.error('Ошибка при загрузке данных:', error);
+      });
+  
+    var imgClients = document.querySelector('.img-clients');
+    var titleClient = document.querySelector('.title-client h2');
+    var local = document.querySelector('.local');
+    var income = document.querySelector('.income');
+    var date = document.querySelector('.date');
+    var life = document.querySelector('.life');
+  
+    var currentClientIndex = 1;
+  
+    function updateContent(clientIndex) {
+      if (clients) {
+        var client = clients['client-' + clientIndex];
+        imgClients.style.backgroundImage = 'url(' + client.img + ')';
+        titleClient.textContent = client.title;
+        local.textContent = client.local;
+        income.textContent = client.income;
+        date.textContent = client.date;
+        life.textContent = client.life;
+      }
+    }
+  
+
+    document.getElementById('left-carret-client').addEventListener('click', function() {
+      if (currentClientIndex > 1) {
+        currentClientIndex--;
+      } else {
+        currentClientIndex = Object.keys(clients).length;
+      }
+      updateContent(currentClientIndex);
+    });
+  
+    document.getElementById('right-carret-client').addEventListener('click', function() {
+      if (currentClientIndex < Object.keys(clients).length) {
+        currentClientIndex++;
+      } else {
+        currentClientIndex = 1;
+      }
+      updateContent(currentClientIndex);
+    });
+  });
