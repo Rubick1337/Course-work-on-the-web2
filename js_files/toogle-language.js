7c32fa9f755276e39a089a12c503c0fdbcb9c357
Rubick1337 <poss1337@gmail.com.>
2024-05-13 18:53:18 +0300

    fix: background selector language

--- js_files/toogle-language.js
+++ js_files/toogle-language.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const dropbtn = document.querySelector(".dropbtn");
-  if (this.location.href.includes("#en")) {
+  if (location.href.includes("#en")) {
     dropbtn.textContent = "EN";
-    dropbtn.style.backgroundImage = "/images/flag-usa-icon.png";
+    dropbtn.style.backgroundImage = 'url("/images/flag-usa-icon.png")';
+  } else if (location.href.includes("#ru")) {
+    dropbtn.textContent = "RU";
+    dropbtn.style.backgroundImage = 'url("/images/Russia-Flag-icon.png")';
   }
-  else if(this.location.href.includes("#en"))
-    {
-      dropbtn.textContent = "RU"
-        dropbtn.style.backgroundImage = "/images/Russia-Flag-icon.png"
-    }
   const dropdownContent = document.querySelector(".dropdown-content");
   dropbtn.addEventListener("click", function () {
     const computedStyle = getComputedStyle(dropdownContent);
